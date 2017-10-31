933d5cedfd7d16e181c74d99c7fd8d2000742c53
Kevin Williams <kevin.williams@algo-fx.net>
2017-10-31 06:43:57 -0400

    10.31.17 6.43am

---
+++ kivyic/pdf.py
@@ -0,0 +1,16 @@
+# Manipulating PDFs with Python
+# https://www.binpress.com/tutorial/manipulating-pdfs-with-python/167
+
+# PyPDF2 Documentation
+# https://pythonhosted.org/PyPDF2/index.html
+
+# http://pages.cs.wisc.edu/~ghost/
+
+
+# https://stackoverflow.com/questions/2002055/converting-pdf-to-images-automatically
+
+# Wand
+# https://stackoverflow.com/questions/37299666/python-converting-pdf-to-image
+
+from wand.image import Image
+

784f01a42f4e9fa6302c84e8a6b3499901b5a5e4
Infinity Cliff <kevin.williams@infinitycliff.com>
2017-11-15 19:51:09 -0500

    Build 0.1.dev3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='kivyic',
-        version='0.1.dev1',
+        version='0.1.dev3',
         packages=['kivyic'],
         package_data={'': ['*.kv']},
         include_package_data=True,
